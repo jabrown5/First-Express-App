69238ca4804cdfda80b81347e2424d7fae509807
Jennifer Brown <j.a.brown.5@gmail.com>
2017-03-01 16:07:58 -0600

    adding more from WDI7

--- app.js
+++ app.js
@@ -22,23 +22,42 @@
 // console.log("server has started");
 
 
+
 // ********************
 // WDI7 server practice
 var express = require("express");
+var path 	= require("path");
+// app is the object we return everytime we invoke express
 var app		= express();
+// http (below) is already a part of node
 var server = require('http').createServer(app);
+var hbs = require('hbs');
 
-app.get('/', function(request, response, next) {
-	// request obj is from the client
-	// the response is what we are sending back
-	response.send('Hey, i just built a server')
+
+// telling express that our views are in ./views
+app.set('views', path.join(__dirname, 'views'));
+// telling epxress that we are using handlebars
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.join(__dirname + '/public')));
+
+
+app.get('/', function(request, response) {
+	response.render('home',{name:"Jen", loggedIn: true, friends:['kirby','benny','lewis']});
 })
 
-app.get('/cats', function(request, response, next) {
-	// request obj is from the client
-	// the response is what we are sending back
-	response.send('Cats run this server!')
-})
+
+// app.get('/', function(request, response, next) {
+// 	// request obj is from the client
+// 	// the response is what we are sending back
+// 	response.send('Hey, i just built a server')
+// })
+
+// app.get('/cats', function(request, response, next) {
+// 	// request obj is from the client
+// 	// the response is what we are sending back
+// 	response.send('Cats run this server!')
+// })
 
 // 1st argument is the port #
 server.listen(3000, function() {
